87e57f4523cfda343548f84a6b217e1d32c26cff
Kaoser <kaosermail@gmail.com>
2022-08-04 17:28:06 +0200

    steam api request2

--- App.tsx
+++ App.tsx
@@ -1,48 +1,91 @@
 import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
+const axios = require('axios');
+
+// function getData()
+// {
+
+  // axios.get('https://api.github.com/zen')
+  //     .then(function (res: any) {
+  //       // handle success
+  //       setResponse(JSON.stringify(res))
+  //       currentResponse = response;
+  //     })
+  //     .catch(function (err: any) {
+  //       // handle error
+  //       console.log(err);
+  //     })
+  //     .then(function () {
+  //     });
+// }
+
+// getData()
+
 
 export default function App() {
   console.log("HEEEEEEEEEEEY")
+  const [response, setResponse] = useState('asdfsq')
+  
+  const WelcomeScreen = () => (
+    <View>
+      <Button title='Hola'>Step One</Button>
+      <Text style={styles.steam}>
+        { response }
+      </Text>
+     </View>
+  );
+
+
+
+  let key = "97B27B336B15D2AF683F9AC509191D6F";
+  let steamid = "76561198154985356";
+
+  // axios.get('https://cors-anywhere.herokuapp.com/http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/', {
+  //   headers: {
+  //     "Access-Control-Allow-Headers": "*"
+  //   }
+  // }).then(function (res: any) {
+  //       if (response != "asdfsq")
+  //       {
+  //         setResponse(JSON.stringify(res))
+  //       }
+  //     });
+
+  axios.get('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=' + key + '&steamid=' + steamid + '&format=json').then(function (res: any) {
+    console.log(res)
+        if (res != "asdfsq")
+        {
+          setResponse(JSON.stringify(res))
+        }
+      });
+  // axios.get('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=' + key + '&steamid=' + steamid + '&format=json').then(function (res: any) {
+  //       if (response != "asdfsq")
+  //       {
+  //         setResponse(JSON.stringify(res))
+  //       }
+  //     });
 
   return (
     <View style={styles.container}>
       <Text>SE VIENEEEEEEEEEEEE</Text>
       <StatusBar style="auto" />
-      <WelcomeScreen></WelcomeScreen>
+      <WelcomeScreen ></WelcomeScreen>
     </View>
   );
 }
 
-const WelcomeScreen = () => (
-  <View>
-    {/* <Header title="Welcome to React Native"/> */}
-    <Button title='Hola'>Step One</Button>
-    <Text>
-      Edit App.js to change this screen and turn it
-      into your app.
-    </Text>
-    <Text >See Your Changes</Text>
-    <Text>
-      Press Cmd + R inside the simulator to reload
-      your app’s code.
-    </Text>
-    <Text>Debug</Text>
-    <Text>
-      Press Cmd + M or Shake your device to open the
-      React Native Debug Menu.
-    </Text>
-    <Text>Learn</Text>
-    <Text>
-      Read the docs to discover what to do next:
-    </Text>
-   </View>
-);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  steam: {
+    flex: 1,
+    backgroundColor: '#FFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
