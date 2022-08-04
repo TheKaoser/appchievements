95be751949f1fc4781f1cd0bdc226bad6ead621b
Kaoser <kaosermail@gmail.com>
2022-08-04 10:40:36 +0200

    first commit

--- App.tsx
+++ App.tsx
@@ -1,19 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
 export default function App() {
+  console.log("HEEEEEEEEEEEY")
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>SE VIENEEEEEEEEEEEE</Text>
       <StatusBar style="auto" />
+      <WelcomeScreen></WelcomeScreen>
     </View>
   );
 }
 
+const WelcomeScreen = () => (
+  <View>
+    {/* <Header title="Welcome to React Native"/> */}
+    <Button title='Hola'>Step One</Button>
+    <Text>
+      Edit App.js to change this screen and turn it
+      into your app.
+    </Text>
+    <Text >See Your Changes</Text>
+    <Text>
+      Press Cmd + R inside the simulator to reload
+      your app’s code.
+    </Text>
+    <Text>Debug</Text>
+    <Text>
+      Press Cmd + M or Shake your device to open the
+      React Native Debug Menu.
+    </Text>
+    <Text>Learn</Text>
+    <Text>
+      Read the docs to discover what to do next:
+    </Text>
+   </View>
+);
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
